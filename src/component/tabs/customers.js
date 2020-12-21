2ed7c09d97f305afd4b11e2706bd9f84b88687a9
Martyn Fitzgerald <martynfitzzz2014@gmail.com>
2020-12-21 22:25:34 +0000

    Adding staff and customer fontend components

--- src/component/tabs/customers.js
+++ src/component/tabs/customers.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { withStyles } from '@material-ui/core/styles';
-import { Typography, Toolbar } from '@material-ui/core';
+import { Typography, Toolbar, Grid, Button } from '@material-ui/core';
+import { DataGrid } from '@material-ui/data-grid';
+
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import SaveIcon from '@material-ui/icons/Save';
 
 const  useStyles = theme => ({
   root: {
@@ -10,7 +15,36 @@ const  useStyles = theme => ({
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  button: {
+    margin: theme.spacing(1),
+  },
 });
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 200 },
+  { field: 'lastName', headerName: 'Last name', width: 200 },
+  { field: 'email', headerName: 'Email', width: 350 },
+  { field: 'phoneNumber', headerName: 'Phone Number', width: 200 },
+  { field: 'lastUpdatedAt', headerName: 'Last Updated At', width: 200 }
+];
+
+const rows = [
+  { id: 1, firstName: 'Jon', lastName: 'Snow',  email: 'Jon.Snow@hotmail.com', phoneNumber: '07123 231323', lastUpdatedAt: '21-12-2020 22:21:35' },
+  { id: 2, firstName: 'Billy', lastName: 'Klen',  email: 'Billy.Klen@hotmail.com', phoneNumber: '07932 645343', lastUpdatedAt: '21-12-2020 22:21:35' },
+  { id: 3, firstName: 'Martyn', lastName: 'Fitzgerald',  email: 'Martyn.Fitz@hotmail.com', phoneNumber: '07212 112233', lastUpdatedAt: '21-12-2020 22:21:35' },
+  { id: 4, firstName: 'Rudi', lastName: 'Tode',  email: 'Rudi.Tode@hotmail.com', phoneNumber: '07212 112233', lastUpdatedAt: '21-12-2020 22:21:35' },
+  { id: 5, firstName: 'Billy', lastName: 'Klen',  email: 'Billy.Klen@hotmail.com', phoneNumber: '07932 645343', lastUpdatedAt: '21-12-2020 22:21:35' },
+  { id: 6, firstName: 'Jon', lastName: 'Snow',  email: 'Jon.Snow@hotmail.com', phoneNumber: '07123 231323', lastUpdatedAt: '21-12-2020 22:21:35' },
+  { id: 7, firstName: 'Martyn', lastName: 'Fitzgerald',  email: 'Martyn.Fitzgerald@hotmail.com', phoneNumber: '07212 112233', lastUpdatedAt: '21-12-2020 22:21:35' },
+  { id: 8, firstName: 'Rudi', lastName: 'White',  email: 'Rudi.Tode@hotmail.com', phoneNumber: '07212 112233', lastUpdatedAt: '21-12-2020 22:21:35' },
+  { id: 9, firstName: 'Martyn', lastName: 'Fitzgerald',  email: 'Martyn.Fitzgerald@hotmail.com', phoneNumber: '07212 112233', lastUpdatedAt: '21-12-2020 22:21:35' },
+  { id: 10, firstName: 'Rudi', lastName: 'Tode',  email: 'Rudi.Tode@hotmail.com', phoneNumber: '07212 112233', lastUpdatedAt: '21-12-2020 22:21:35' },
+  { id: 11, firstName: 'Martyn', lastName: 'Fitzgerald',  email: 'Martyn.Fitzgerald@hotmail.com', phoneNumber: '07212 112233', lastUpdatedAt: '21-12-2020 22:21:35' },
+  { id: 12, firstName: 'Rudi', lastName: 'White',  email: 'Rudi.Tode@hotmail.com', phoneNumber: '07212 112233', lastUpdatedAt: '21-12-2020 22:21:35' },
+  { id: 13, firstName: 'Martyn', lastName: 'Fitzgerald',  email: 'Martyn.Fitzgerald@hotmail.com', phoneNumber: '07212 112233', lastUpdatedAt: '21-12-2020 22:21:35' },
+  { id: 14, firstName: 'Rudi', lastName: 'Tode',  email: 'Rudi.Tode@hotmail.com', phoneNumber: '07212 112233', lastUpdatedAt: '21-12-2020 22:21:35' },
+];
 
 class Customers extends React.Component {
   render() {
@@ -20,10 +54,48 @@ class Customers extends React.Component {
       <div className={classes.root}>
         <main className={classes.content}>
           <Toolbar />
-          <Typography paragraph>
-            Customers
-          </Typography>
-        </main>
+          <Grid container spacing={1}>
+            <Grid container xs={12} sm={6}>
+              <Typography variant="h4" gutterBottom>
+                Customers
+              </Typography>
+            </Grid>
+
+            <Grid container  xs={12} sm={6} justify="flex-end" >
+              <Button
+                variant="contained"
+                color="default"
+                className={classes.button}
+                startIcon={<AddIcon />}
+              >
+                Add
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                className={classes.button}
+                startIcon={<SaveIcon />}
+              >
+                Save
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                className={classes.button}
+                startIcon={<DeleteIcon />}
+              >
+                Remove
+              </Button>
+            </Grid>
+            
+            <Grid item xs={12}>
+              <div style={{ height: 650, width: '100%' }}>
+                <DataGrid rows={rows} columns={columns} pageSize={10} checkboxSelection />
+              </div>
+            </Grid>
+          </Grid>
+        </main> 
       </div>
     );
   }
--- src/component/tabs/staffMembers.js
+++ src/component/tabs/staffMembers.js
@@ -4,7 +4,7 @@ import { Typography, Toolbar, Grid, Button } from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
 
 import DeleteIcon from '@material-ui/icons/Delete';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import AddIcon from '@material-ui/icons/Add';
 import SaveIcon from '@material-ui/icons/Save';
 
 const  useStyles = theme => ({
@@ -22,38 +22,28 @@ const  useStyles = theme => ({
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-  },
+  { field: 'firstName', headerName: 'First name', width: 200 },
+  { field: 'lastName', headerName: 'Last name', width: 200 },
+  { field: 'email', headerName: 'Email', width: 350 },
+  { field: 'phoneNumber', headerName: 'Phone Number', width: 200 },
+  { field: 'permissionLevel', headerName: 'Permission Level', width: 200 }
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  { id: 10, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 11, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 11, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, firstName: 'Jon', lastName: 'Snow',  email: 'Jon.Snow@hotmail.com', phoneNumber: '07123 231323', permissionLevel: '1' },
+  { id: 2, firstName: 'Billy', lastName: 'Klen',  email: 'Billy.Klen@hotmail.com', phoneNumber: '07932 645343', permissionLevel: '2' },
+  { id: 3, firstName: 'Martyn', lastName: 'Fitzgerald',  email: 'Martyn.Fitz@hotmail.com', phoneNumber: '07212 112233', permissionLevel: '3' },
+  { id: 4, firstName: 'Rudi', lastName: 'Tode',  email: 'Rudi.Tode@hotmail.com', phoneNumber: '07212 112233', permissionLevel: '3' },
+  { id: 5, firstName: 'Billy', lastName: 'Klen',  email: 'Billy.Klen@hotmail.com', phoneNumber: '07932 645343', permissionLevel: '2' },
+  { id: 6, firstName: 'Jon', lastName: 'Snow',  email: 'Jon.Snow@hotmail.com', phoneNumber: '07123 231323', permissionLevel: '1' },
+  { id: 7, firstName: 'Martyn', lastName: 'Fitzgerald',  email: 'Martyn.Fitzgerald@hotmail.com', phoneNumber: '07212 112233', permissionLevel: '3' },
+  { id: 8, firstName: 'Rudi', lastName: 'White',  email: 'Rudi.Tode@hotmail.com', phoneNumber: '07212 112233', permissionLevel: '3' },
+  { id: 9, firstName: 'Martyn', lastName: 'Fitzgerald',  email: 'Martyn.Fitzgerald@hotmail.com', phoneNumber: '07212 112233', permissionLevel: '3' },
+  { id: 10, firstName: 'Rudi', lastName: 'Tode',  email: 'Rudi.Tode@hotmail.com', phoneNumber: '07212 112233', permissionLevel: '3' },
+  { id: 11, firstName: 'Martyn', lastName: 'Fitzgerald',  email: 'Martyn.Fitzgerald@hotmail.com', phoneNumber: '07212 112233', permissionLevel: '3' },
+  { id: 12, firstName: 'Rudi', lastName: 'White',  email: 'Rudi.Tode@hotmail.com', phoneNumber: '07212 112233', permissionLevel: '3' },
+  { id: 13, firstName: 'Martyn', lastName: 'Fitzgerald',  email: 'Martyn.Fitzgerald@hotmail.com', phoneNumber: '07212 112233', permissionLevel: '3' },
+  { id: 14, firstName: 'Rudi', lastName: 'Tode',  email: 'Rudi.Tode@hotmail.com', phoneNumber: '07212 112233', permissionLevel: '3' },
 ];
 
 class StaffMembers extends React.Component {
@@ -65,28 +55,20 @@ class StaffMembers extends React.Component {
         <main className={classes.content}>
           <Toolbar />
           <Grid container spacing={1}>
-            <Grid item xs={12} sm={6}>
+            <Grid container xs={12} sm={6}>
               <Typography variant="h4" gutterBottom>
                 Staff Members
               </Typography>
             </Grid>
 
-            <Grid item xs={12} sm={6}>
-              <Button
-                variant="contained"
-                color="secondary"
-                className={classes.button}
-                startIcon={<DeleteIcon />}
-              >
-                Delete
-              </Button>
+            <Grid container  xs={12} sm={6} justify="flex-end" >
               <Button
                 variant="contained"
                 color="default"
                 className={classes.button}
-                startIcon={<CloudUploadIcon />}
+                startIcon={<AddIcon />}
               >
-                Upload
+                Add
               </Button>
               <Button
                 variant="contained"
@@ -96,6 +78,14 @@ class StaffMembers extends React.Component {
                 startIcon={<SaveIcon />}
               >
                 Save
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                className={classes.button}
+                startIcon={<DeleteIcon />}
+              >
+                Remove
               </Button>
             </Grid>
             
