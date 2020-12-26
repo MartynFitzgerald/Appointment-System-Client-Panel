e8d54a39edff0f2fcdf330e3bd35622ca7a4244c
Martyn Fitzgerald <martynfitzzz2014@gmail.com>
2020-12-26 20:05:28 +0000

    Adding scheduler

--- src/component/tabs/appointments.js
+++ src/component/tabs/appointments.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { withStyles } from '@material-ui/core/styles';
-import { Typography, Toolbar, Grid } from '@material-ui/core';
+import { Typography, Toolbar, Grid, Button } from '@material-ui/core';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import { Scheduler, DayView, Appointments } from '@devexpress/dx-react-scheduler-material-ui';
+
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
 
 const  useStyles = theme => ({
   root: {
@@ -10,9 +16,18 @@ const  useStyles = theme => ({
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  button: {
+    margin: theme.spacing(1),
+  },
 });
 
-class Appointments extends React.Component {
+const currentDate = '2018-11-01';
+const schedulerData = [
+  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
+  { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
+];
+
+class AppointmentsClass extends React.Component {
   render() {
     const { classes } = this.props;
 
@@ -28,11 +43,47 @@ class Appointments extends React.Component {
             </Grid>
 
             <Grid container  xs={12} sm={6} justify="flex-end" >
-
+              <Button
+                variant="contained"
+                color="default"
+                className={classes.button}
+                startIcon={<AddIcon />}
+              >
+                Add
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                size="small"
+                className={classes.button}
+                startIcon={<EditIcon />}
+              >
+                Modify
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                className={classes.button}
+                startIcon={<DeleteIcon />}
+              >
+                Remove
+              </Button>
             </Grid>
             
             <Grid item xs={12}>
 
+            <Scheduler
+              data={schedulerData}
+            >
+              <ViewState
+                currentDate={currentDate}
+              />
+              <DayView
+                startDayHour={9}
+                endDayHour={14}
+              />
+              <Appointments />
+            </Scheduler>
             </Grid>
           </Grid>
         </main> 
@@ -41,4 +92,4 @@ class Appointments extends React.Component {
   }
 }
 
-export default withStyles(useStyles, { withTheme: true })(Appointments);
+export default withStyles(useStyles, { withTheme: true })(AppointmentsClass);
